88b75c11a04ac1f02198a9fe9b4045113cb29b8d
harshsahu0499 <harshsahu0499@gmail.com>
2018-10-10 11:50:48 +0530

    Harsh

---
+++ harsh59/uc.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+
+int age;
+char ms,gender
+printf("enter age merital status gender");
+scanf("%d %c %c",age,ms,gender);
+if(ms=='m');
+printf("driver is insured and married");
+else if(gender=='m'&&age>30)
+printf("driver is insured");
+else if(age>25);
+printf("driver is insured");
+else
+printf("driver is not insured");
+return 0;
+}   
+
+
+
