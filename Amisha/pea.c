d0e2bfc55b5117ad40569781929956ff501c2b6f
amisha013 <amisha.keshariya13@gmail.com>
2018-10-10 11:51:08 +0530

    amisha

---
+++ Amisha/pea.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+	int x,y;
+
+	 int sum(int x,int y,int i);
+         printf("the sum is::%d" ,y);
+}
+
+        int sum(int x,int y,int i)
+        {
+	 printf("Enter a number x:: ");
+	 scanf("%d", &x);
+         for(i=1;i<=x;i++)
+         {
+	   y=y+i;
+          }
+	   return y;
+        }
+		
+          
+            
+	      
+	         	      
+	     
