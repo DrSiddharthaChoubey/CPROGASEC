5bc32b30c88b6fa65db0bc5a79d252b68241841c
Bhawana15 <bhawana.swarna@gmail.com>
2018-10-10 11:50:13 +0530

    Bhawana

---
+++ Bhawana/demo.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int a,i,count;
+for(i=2;i<=(sqrt(a));i++)
+{
+if(a%i==0)
+count++;
+}
+if(count==0)
+printf("the no. is prime");
+else
+printf("the no is not a prime ");
+
+return 0;
+}  
